8ffc38d4bc3c02d0d42c459c3a08818bef41919c
eish <eish@spotdraft.com>
2024-12-14 23:51:31 +0530

    resetup

--- dist/interaction/types.js
+++
@@ -1,6 +0,0 @@
-export var InteractionType;
-(function (InteractionType) {
-    InteractionType["CLICK"] = "click";
-    InteractionType["INPUT"] = "input";
-    InteractionType["FILE_UPLOAD"] = "fileUpload";
-})(InteractionType || (InteractionType = {}));
